5579fc645b1dd4755662b7e4fb7abe4f74034cea
hichambouzid <bouzidhicham51@gmail.com>
2024-08-28 12:55:53 +0100

    PUSH-0.0.0: push ex00, no need of oop knowledge in ex00

---
+++ CPP-00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hibouzid <hibouzid@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/27 16:01:22 by hibouzid          #+#    #+#             */
+/*   Updated: 2024/08/28 12:52:27 by hibouzid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (1);
+	}
+	for (int j = 1; j < ac; j++)
+		for (int i = 0; av[j][i]; i++)
+			putchar(toupper(av[j][i]));
+	std::cout << '\n';
+}
