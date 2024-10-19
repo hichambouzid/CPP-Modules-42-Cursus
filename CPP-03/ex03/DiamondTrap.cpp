46979864b0486e1ee0314af6228db08bf50819c8
hichambouzid <bouzidhicham51@gmail.com>
2024-10-19 18:09:21 +0100

    PUSH-0.2.9: Add a messing message.

--- CPP-03/ex03/DiamondTrap.cpp
+++ CPP-03/ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: hibouzid <hibouzid@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 21:46:21 by hibouzid          #+#    #+#             */
-/*   Updated: 2024/10/19 17:55:52 by hibouzid         ###   ########.fr       */
+/*   Updated: 2024/10/19 18:08:12 by hibouzid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,21 @@ DiamondTrap::DiamondTrap() : ClapTrap("Random_clap_name")
 {
 	std::cout << "Default Constructor of DiamondTrap class called\n";
 	this->Name = "Random";
+	this->Attak_Damage = 30; 
 }
 
 DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name")
 {
 	std::cout << "constructor DiamondTrap called\n";
 	this->Name = name;
-	this->Hit_Points = FragTrap::Hit_Points;
-	this->Enrgy_Points = ScavTrap::Enrgy_Points;
 	this->Attak_Damage = 30;
 }
 
 void DiamondTrap::whoAmI()
 {
-	std::cout << "whoAmI DiamondTrap: " << this->Name << " & Hit point: " << this->Hit_Points << " Energy_point: " << this->Enrgy_Points
+	std::cout << "whoAmI DiamondTrap: " << this->Name << " ,ClapTrap: " << ClapTrap::Name << " & Hit point: " << this->Hit_Points << " Energy_point: " << this->Enrgy_Points
 	<< " attack_damage: " << this->Attak_Damage << '\n';
-	this->attack("attack_Looo");
+	// this->attack("attack_Looo");
 }
 
 DiamondTrap::~DiamondTrap()
