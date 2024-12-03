49244689199449b90928340316063cda1ebfb40b
hichambouzid <bouzidhicham51@gmail.com>
2024-12-03 23:34:50 +0100

    PUSH-0.6.2: push to check in linux version .

--- CPP-09/ex02/PmergeMe.cpp
+++ CPP-09/ex02/PmergeMe.cpp
@@ -1,10 +1,12 @@
 #include "PmergeMe.hpp"
 
+std::queue<int> FirstData;
+std::list<int> SecondData;
+
 std::queue<int> mergeSortQueue(std::queue<int> &left, std::queue<int> &right)
 {
 	std::queue<int> result;
 
-	std::cout << "left size: " << left.size() << " right size: " << right.size() << '\n';
 	while (!left.empty() && !right.empty())
 	{
 		if (left.front() > right.front())
@@ -57,11 +59,27 @@ std::queue<int> splitQueue(std::queue<int> &mainq)
 	return (mergeSortQueue(left, right));
 }
 
+long	get_current_time(void)
+{
+	struct timeval	time;
+	long			nbr;
+
+	if (gettimeofday(&time, NULL))
+	{
+		std::cerr << "Error: was happen ind gettimeofday\n";
+		return (-1);
+	}
+	nbr = time.tv_sec * 1000000;
+	nbr += time.tv_usec;
+	return (nbr);
+}
+
+
 std::list<int> mergeSortList(std::list<int> &left, std::list<int> &right)
 {
 	std::list<int> result;
 
-	std::cout << "left size: " << left.size() << " right size: " << right.size() << '\n';
+	// std::cout << "left size: " << left.size() << " right size: " << right.size() << '\n';
 	while (!left.empty() && !right.empty())
 	{
 		if (left.front() > right.front())
@@ -112,4 +130,46 @@ std::list<int> splitList(std::list<int> &mainq)
 	left = splitList(left);
 	right = splitList(right);
 	return (mergeSortList(left, right));
+}
+
+void SortData()
+{
+	double end, start;
+	start = get_current_time();
+	SecondData = splitList(SecondData);
+	end = get_current_time();
+	std::cout << "After:  " ;
+	for (std::list<int>::iterator it = SecondData.begin(); it != SecondData.end(); it++)
+		std::cout << ' ' << *it;
+	std::cout << '\n';
+	std::cout << "Time to process a range of " << SecondData.size() <<
+		" elements with std::list : " << (end - start) / 1000 << " us\n";
+	start = get_current_time();
+	FirstData = splitQueue(FirstData);
+	end = get_current_time();
+	std::cout << "Time to process a range of " << FirstData.size() <<
+		" elements with std::queue : " << (end - start) / 1000 << " us\n";
+}
+
+void fillData(int ac, char **av)
+{
+	char *str;
+	long number;
+
+	for (int i = 1; i < ac; i++)
+	{
+		number = strtod(av[i], &str);
+		if (*str || number < 0 || number > 2147483647)
+		{
+			std::cerr << "Error: invalide number .\n";
+			exit(-1);
+		}
+		FirstData.push(number);
+		SecondData.push_back(number);
+	}
+	std::cout << "Before: " ;
+	for (std::list<int>::iterator it = SecondData.begin(); it != SecondData.end(); it++)
+		std::cout << ' ' << *it;
+	std::cout << '\n';
+	SortData();
 }
--- CPP-09/ex02/PmergeMe.hpp
+++ CPP-09/ex02/PmergeMe.hpp
@@ -7,9 +7,17 @@
 #include <string.h>
 #include <deque>
 #include <list>
+#include <ctime>
+#include <time.h>
+#include <unistd.h>
+# include <sys/time.h>
+
 
 std::queue<int> mergeSortQueue(std::queue<int> &left, std::queue<int> &right);
 std::queue<int> splitQueue(std::queue<int> &mainq);
 std::list<int> mergeSortList(std::list<int> &left, std::list<int> &right);
 std::list<int> splitList(std::list<int> &mainq);
+
+void fillData(int ac, char **av);
+long	get_current_time(void);
 #endif
